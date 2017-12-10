c0fc035c8a25b0ca30d6c247b7170af40470d903
MstRobotics <tspivey606@gmail.com>
2017-12-10 13:50:18 -0600

    seperated some code into several files for organization

---
+++ catkin_ws/src/ROV_system/src/cp_joystick_functions.cpp
@@ -0,0 +1,169 @@
+#ifndef CP_JOYSTICK_FUNCTIONS
+#define CP_JOYSTICK_FUNCTIONS
+
+//process data from joystick
+void joystick_cb(const sensor_msgs::Joy &joy)
+{
+  float angle = atan2(joy.axes[axis_stick_y],  joy.axes[axis_stick_x]*-1);
+  float magnitude = sqrt(pow(joy.axes[axis_stick_y],2) + pow(joy.axes[axis_stick_x],2));
+
+  int trigger = 0;
+  int pinky = 0;
+
+  angle *= 180/M_PI;
+
+  if(angle < 0)
+  {
+    angle += 360;
+  }
+
+  if(magnitude > 1.0)
+  {
+    magnitude = 1.0;
+  }
+  if(magnitude < 0.0)
+  {
+    magnitude = 0.0;
+  }
+
+  axis_stick_x_value.data = magnitude;
+  axis_stick_y_value.data = angle;
+  
+  axis_stick_rotation_value.data = joy.axes[axis_stick_rotation];
+
+
+
+  if(joy.buttons[button_trigger] == 1)
+  {
+    ROS_INFO("Trigger pulled");
+    button_trigger_state.data = 1;
+    trigger = button_trigger_state.data;
+    //trigger = 1;
+  }
+  else
+    button_trigger_state.data = 0;
+
+
+  if(joy.buttons[button_pinky_trigger] == 1)
+  {
+    ROS_INFO("Button pinky trigger pressed");
+    button_pinky_trigger_state.data = 1;
+    pinky = 1;
+  }
+  else
+    button_pinky_trigger_state.data = 0;
+
+
+
+
+
+
+/*
+ //buttons are stored in joy.buttons[]
+  //axes are stored in joy.axis[]
+
+
+  if(joy.buttons[button_a] == 1)
+  {
+    ROS_INFO("Button A pressed");
+    button_a_state.data = 1;
+  }
+  else
+    button_a_state.data = 0;
+
+
+  if(joy.buttons[button_b] == 1)
+  {
+    ROS_INFO("Button B pressed");
+    button_b_state.data = 1;
+  }
+  else
+    button_b_state.data = 0;
+
+  if(joy.buttons[button_c] == 1)
+  {
+    ROS_INFO("Button C pressed");
+    button_c_state.data = 1;
+  }
+  else
+    button_c_state.data = 0;
+
+  if(joy.buttons[button_d] == 1)
+  {
+    ROS_INFO("Button D pressed");
+    button_d_state.data = 1;
+  }
+  else
+    button_d_state.data = 0;
+
+
+  if(joy.buttons[button_h1_up] == 1)
+  {
+    ROS_INFO("Button h1 up pressed");
+    button_h1_up_state.data = 1;
+  }
+  else
+    button_h1_up_state.data = 0;
+
+  if(joy.buttons[button_h1_right] == 1)
+  {
+    ROS_INFO("Button h1 right pressed");
+    button_h1_right_state.data = 1;
+  }
+  else
+    button_h1_right_state.data = 0;
+
+  if(joy.buttons[button_h1_down] == 1)
+  {
+    ROS_INFO("Button h1 down pressed");
+    button_h1_down_state.data = 1;
+  }
+  else
+    button_h1_down_state.data = 0;
+
+  if(joy.buttons[button_h1_left] == 1)
+  {
+    ROS_INFO("Button h1 left pressed");
+    button_h1_left_state.data = 1;
+  }
+  else
+    button_h1_left_state.data = 0;
+
+
+  if(joy.buttons[button_h2_up] == 1)
+  {
+    ROS_INFO("Button h2 up pressed");
+    button_h2_up_state.data = 1;
+  }
+  else
+    button_h2_up_state.data = 0;
+
+  if(joy.buttons[button_h2_right] == 1)
+  {
+    ROS_INFO("Button h2 right pressed");
+    button_h2_right_state.data = 1;
+  }
+  else
+    button_h2_right_state.data = 0;
+
+  if(joy.buttons[button_h2_down] == 1)
+  {
+    ROS_INFO("Button h2 down pressed");
+    button_h2_down_state.data = 1;
+  }
+  else
+    button_h2_down_state.data = 0;
+
+  if(joy.buttons[button_h2_left] == 1)
+  {
+    ROS_INFO("Button h2 left pressed");
+    button_h2_left_state.data = 1;
+  }
+  else
+    button_h2_left_state.data = 0;
+
+  axis_pov_x_value.data = joy.axes[axis_pov_x];
+  axis_pov_y_value.data = joy.axes[axis_pov_y];
+*/
+}
+#endif
---
+++ catkin_ws/src/ROV_system/src/messages.h
@@ -0,0 +1,101 @@
+#ifndef MESSAGES_H
+#define MESSAGES_H
+
+#include <std_msgs/Int16.h>
+
+std_msgs::Float32 axis_stick_x_value;
+std_msgs::Float32 axis_stick_y_value;
+std_msgs::Float32 axis_stick_rotation_value;
+std_msgs::Bool button_trigger_state;
+std_msgs::Bool button_pinky_trigger_state;
+std_msgs::Int16 motor1_value;
+std_msgs::Int16 motor2_value;
+std_msgs::Int16 motor3_value;
+std_msgs::Int16 motor4_value;
+/*
+____________TESTED_____________
+
+std_msgs::Bool button_a_state;
+std_msgs::Bool button_e_state;
+std_msgs::Float32 rotary_4_value;
+std_msgs::Float32 rotary_3_value;
+
+std_msgs::Float32 axis_joystick_thumb_stick_x_value;
+std_msgs::Float32 axis_joystick_thumb_stick_y_value;
+
+std_msgs::Float32 axis_pov_x_value;
+std_msgs::Float32 axis_pov_y_value;
+
+std_msgs::Bool button_b_state;
+std_msgs::Bool button_c_state;
+std_msgs::Bool button_d_state;
+std_msgs::Bool button_pinky_trigger_state;
+
+std_msgs::Bool button_h1_up_state;
+std_msgs::Bool button_h1_right_state;
+std_msgs::Bool button_h1_down_state;
+std_msgs::Bool button_h1_left_state;
+
+std_msgs::Bool button_h2_up_state;
+std_msgs::Bool button_h2_right_state;
+std_msgs::Bool button_h2_down_state;
+std_msgs::Bool button_h2_left_state;
+
+std_msgs::Float32 axis_left_thruster_value;
+std_msgs::Float32 axis_right_thruster_value;
+
+std_msgs::Float32 axis_rotary_f_value;
+std_msgs::Float32 axis_rotary_g_value;
+
+std_msgs::Float32 axis_thruster_thumb_stick_x_value;
+std_msgs::Float32 axis_thruster_thumb_stick_y_value;
+
+std_msgs::Bool button_rotary_f_state;
+std_msgs::Bool button_rotary_g_state;
+
+std_msgs::Bool button_i_state;
+std_msgs::Bool button_h_state;
+
+std_msgs::Bool button_sw1_state;
+std_msgs::Bool button_sw2_state;
+std_msgs::Bool button_sw3_state;
+std_msgs::Bool button_sw4_state;
+std_msgs::Bool button_sw5_state;
+std_msgs::Bool button_sw6_state;
+
+std_msgs::Bool button_tgl1_up_state;
+std_msgs::Bool button_tgl1_down_state;
+
+std_msgs::Bool button_tgl2_up_state;
+std_msgs::Bool button_tgl2_down_state;
+
+std_msgs::Bool button_tgl3_up_state;
+std_msgs::Bool button_tgl3_down_state;
+
+std_msgs::Bool button_tgl4_up_state;
+std_msgs::Bool button_tgl4_down_state;
+
+std_msgs::Bool button_h3_up_state;
+std_msgs::Bool button_h3_right_state;
+std_msgs::Bool button_h3_down_state;
+std_msgs::Bool button_h3_left_state;
+
+std_msgs::Bool button_h4_up_state;
+std_msgs::Bool button_h4_right_state;
+std_msgs::Bool button_h4_down_state;
+std_msgs::Bool button_h4_left_state;
+
+std_msgs::Bool button_ki_up_state;
+std_msgs::Bool button_ki_down_state;
+
+std_msgs::Bool button_thumb_stick_state;
+std_msgs::Bool button_thumb_slider_state;
+
+std_msgs::Bool button_scroll_forward_state;
+std_msgs::Bool button_scroll_reverse_state;
+
+std_msgs::Bool button_m1_state;
+std_msgs::Bool button_m2_state;
+std_msgs::Bool button_s1_state;
+*/
+#endif
