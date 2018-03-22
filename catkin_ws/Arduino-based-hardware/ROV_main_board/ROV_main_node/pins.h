0ca0c6a7aecb77e6cfaffea55a0913e17ab8223e
WannaBAcoder <vgmcn3@mst.edu>
2018-03-22 17:53:24 -0500

    added support for new boards

--- catkin_ws/Arduino-based-hardware/ROV_main_board/ROV_main_node/pins.h
+++ catkin_ws/Arduino-based-hardware/ROV_main_board/ROV_main_node/pins.h
@@ -5,7 +5,7 @@
  *
  * 
  *Description: 	This file defines pin names and numbers 
- *   		for use in a .ino file
+ *   		for use in the main .ino file
  */
 
 
@@ -13,24 +13,6 @@
 #define PINS_H
 
 
-//servo and thruster pins
-
-  #define flt_pin 2
-  #define middle_left_pin 3
-  #define blt_pin 4
-  #define frt_pin 7
-  #define middle_right_pin 6
-  #define brt_pin 5
-
-
-//#define servo3_pin 3
-//joystick x-axis
-#define servo4_pin 8
-//joystick y-axis
-
-//led pins
-//#define led1 13
-//#define led2 8
 
 
 #endif
--- catkin_ws/Arduino-based-hardware/ROV_main_setup/examples/PWM_controller_usage/PWM_controller_usage.ino
+++ catkin_ws/Arduino-based-hardware/ROV_main_setup/examples/PWM_controller_usage/PWM_controller_usage.ino
@@ -2,31 +2,36 @@
  * Author: Vinnie Marco
  * Email: vgmcn3@mst.edu
  * 
- * Date: 10-10-2017
+ * Date: 3-21-2018
  * 
  * Description: Basic usage for onboard 
  *      external servo controller
+ *      
+ * NOTE: Motor controller PCB channel implemetations can be
+ *      found in "pwm_channels.h"
  */
 
+/*
+ * Inlucdes setup for:
+ *  IMU hardware on mainboard
+ *  Motor controller hardware on motor_driver_board
+ */
 #include <ROV_main_setup.h>
-
-//define pin 7 as a servo pin
-#define servo 7
 
 void setup() 
 {
-  bool error_flag = main_setup();//check for an error
-  if(error_flag)
+  bool error_flag = main_setup();//initalize the imu sensor
+  if(error_flag)//check for an IMU error
   {
     //Serial.println("ERROR");//prints error message if serial is started
-    while(1);
+    while(1);//halt if error
   }
 }
 
 void loop() 
 {  
-  external_servo.writeMicroseconds(servo, 1000);//max one direction
+  pwm_primary.writeMicroseconds(gimbal_x, 1000);//max one direction
   delay(2000);
-  external_servo.writeMicroseconds(servo, 2000);//max other direction
+  pwm_primary.writeMicroseconds(gimbal_x, 2000);//max other direction
   delay(2000);
 }
